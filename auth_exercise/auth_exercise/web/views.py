3a2079966e77bb9608917e88c9b8854fe0303c89
GalkaKG <galina_georgieva_net@abv.bg>
2023-07-04 10:48:24 +0300

    new commit

--- auth_exercise/auth_exercise/web/views.py
+++ auth_exercise/auth_exercise/web/views.py
@@ -1,11 +1,37 @@
+from django import forms
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic as views
-from django.contrib.auth import forms as auth_forms
+from django.contrib.auth import forms as auth_forms, login, authenticate
+from django.utils.translation import gettext_lazy as _
+
+
+class RegisterUserForm(auth_forms.UserCreationForm):
+    content = forms.BooleanField()
+
+    password2 = forms.CharField(
+        label=_("Repeat password"),
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+        strip=False,
+        help_text=_("Repeat password, please"),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].help_text = 'It works'
 
 
 class RegisterUserView(views.CreateView):
     template_name = 'register.html'
-    form_class = auth_forms.UserCreationForm
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('register_user')
+
+    def form_valid(self, form):
+        result = super().form_valid(form)
+
+        login(self.request, self.object)
+
+        return result
 
 
 class LoginUserView(views.View):
